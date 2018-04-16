6a5bd9c77c003c0b04da4df22143220c02fc25e2
Mathias Hillesheim <mathiashls@protonmail.com>
2018-04-15 22:31:04 -0300

    Added file to store bot quotes

---
+++ modules/quotes.py
@@ -0,0 +1,5 @@
+BYNEBOT_INVALID_INPUT = [
+    "I'm not that smart! Take it easy on me and type it correctly.",
+    "You what? I have no idea what you meant!",
+    "English please, I don't speak London!",
+]
